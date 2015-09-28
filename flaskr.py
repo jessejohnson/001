e313545aa71dca7292d58a5513a15a8c9de98297
wsgeorge <williamsaintgeorge@gmail.com>
2015-09-28 21:47:24 +0000

    fix indentation error in app.run

--- flaskr.py
+++ flaskr.py
@@ -17,5 +17,5 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-	if __name__ == '__main__':
-		app.run()
+if __name__ == '__main__':
+	app.run()
